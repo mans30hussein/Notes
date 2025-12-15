9bcccb45bd3c9e6f11d666d638c1331d2d70b978
mohamed_hussein <mohamedhusseinmans@gmail.com>
2025-12-15 21:15:35 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:hive_app/view/notes_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      home: const NotesView(),
+    );
+  }
+}
